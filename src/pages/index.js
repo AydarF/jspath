01de146d7c7ffe25a8b95f7d955410975123f85b
Aydar Fayzullin <aydar_fayzullin@yahoo.com>
2019-05-06 00:07:39 -0400

    Add basic styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,22 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Develop and build the future</h1>
+        <p>The world of new technologies awaits you</p>
+        {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Image />
+        </div> */}
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
